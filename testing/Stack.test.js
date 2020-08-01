c9ed0eb3e4ba88d4ab0f182ed327f350ab5f0645
tudorfis <todorescu.consulting@gmail.com>
2020-08-01 16:43:38 +0300

    .

--- testing/Stack.test.js
+++ testing/Stack.test.js
@@ -1,22 +1,3 @@
-
-import Stack from "../lib/data-structures/Stack.js";
-import UnitTesting from "../lib/unit-testing/UnitTesting.js";
-
-let stack
-
-const unitTesting = new UnitTesting({
-    testName: 'Stack.js',
-    testDescription: 'testing the implementation of a stack in javascript' 
-})
-
-unitTesting.before(function() {
-    stack = new Stack()
-
-    stack.push(1)
-    stack.push(2)
-    stack.push(3)
-})
-
 /*
     TESTS TO MATCH
     =================================
@@ -31,61 +12,67 @@ unitTesting.before(function() {
     console.log('pop', stack.pop())
 */
 
-unitTesting.addAssertion('stack.count() === 3', {
-    resultWas: stack.count(),
-    resultExpected: 3,
-    get evaluation() { return (
-        this.resultWas === this.resultExpected
-    )}
+import Stack from "../lib/data-structures/Stack.js";
+import UnitTesting from "../lib/unit-testing/UnitTesting.js";
+
+const buildAssertionDetails = _ => ({ resultWas, resultExpected, ...evaluationObj })
+const buildAddAssertion = assertionName => unitTesting.addAssertion( assertionName, buildAssertionDetails())
+
+let resultWas, resultExpected
+const evaluationObj = {
+    get evaluation() { 
+        return this.resultWas === this.resultExpected
+    }
+}
+
+///////////////////////////////////////////////////////////
+
+let stack
+
+const unitTesting = new UnitTesting({
+    testName: 'Stack.js',
+    testDescription: 'testing the implementation of a stack in javascript' 
 })
 
-unitTesting.addAssertion('stack.pop() === 3', {
-    resultWas: stack.pop(),
-    resultExpected: 3,
-    get evaluation() { return (
-        this.resultWas === this.resultExpected
-    )}
+unitTesting.before(function() {
+    stack = new Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push(3)
+    
 })
+    
+resultWas = stack.count()
+resultExpected = 3
+buildAddAssertion('stack.count() === 3')
 
-unitTesting.addAssertion('stack.count() === 2', {
-    resultWas: stack.count(),
-    resultExpected: 2,
-    get evaluation() { return (
-        this.resultWas === this.resultExpected
-    )}
-})
+resultWas = stack.pop()
+resultExpected = 3
+buildAddAssertion('stack.pop() === 3')
 
-unitTesting.addAssertion('stack.pop() === 2', {
-    resultWas: stack.pop(),
-    resultExpected: 2,
-    get evaluation() { return (
-        this.resultWas === this.resultExpected
-    )}
-})
+resultWas = stack.count()
+resultExpected = 2
+buildAddAssertion('stack.count() === 2')
 
-unitTesting.addAssertion('stack.count() === 1', {
-    resultWas: stack.count(),
-    resultExpected: 1,
-    get evaluation() { return (
-        this.resultWas === this.resultExpected
-    )}
-})
+resultWas = stack.pop()
+resultExpected = 2
+buildAddAssertion('stack.pop() === 2')
 
-unitTesting.addAssertion('stack.pop() === 1', {
-    resultWas: stack.pop(),
-    resultExpected: 1,
-    get evaluation() { return (
-        this.resultWas === this.resultExpected
-    )}
-})
+resultWas = stack.count()
+resultExpected = 1
+buildAddAssertion('stack.count() === 1')
 
-unitTesting.addAssertion('stack.pop() === undefined', {
-    resultWas: stack.pop(),
-    resultExpected: undefined,
-    get evaluation() { return (
-        this.resultWas === this.resultExpected
-    )}
-})
+resultWas = stack.pop()
+resultExpected = 1
+buildAddAssertion('stack.pop() === 1')
+
+resultWas = stack.pop()
+resultExpected = undefined
+buildAddAssertion('stack.pop() === undefined')
+
+resultWas = stack.pop()
+resultExpected = undefined
+buildAddAssertion('stack.pop() === undefined')
 
 unitTesting.getResults()
 
