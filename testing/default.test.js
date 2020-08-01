97c34f2d59e814c0d407b502c60909828ca86c34
tudorfis <todorescu.consulting@gmail.com>
2020-08-01 22:26:58 +0300

    .

---
+++ testing/default.test.js
@@ -0,0 +1,30 @@
+import consoleColors from "../lib/global/console.colors.js"
+
+console.log(`
+
+
+`)
+
+console.log(
+    consoleColors.bg.Blue,
+    consoleColors.fg.White,
+`                                                                                              `,
+    consoleColors.Reset
+)
+
+console.log(
+    consoleColors.bg.Blue,
+    consoleColors.fg.White,
+    `   STARTED TEST AT [ ${new Date()} ]       `,
+    consoleColors.Reset
+)
+
+console.log(
+    consoleColors.bg.Blue,
+    consoleColors.fg.White,
+`                                                                                              `,
+    consoleColors.Reset
+)
+
+console.log(`
+`)
