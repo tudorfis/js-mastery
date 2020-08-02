40a8953c33e26ddeadca1900c670cd66277e2376
tudorfis <todorescu.consulting@gmail.com>
2020-08-03 00:18:38 +0300

    .

--- testing/default.test.js
+++ testing/default.test.js
@@ -1,30 +1,38 @@
 import consoleColors from "../lib/global/console.colors.js"
 
-console.log(`
+const defaultSeparatorSpace = `                                                                                              `
+const defaultMessage = `   STARTED TEST AT [ ${new Date()} ]       `
+const newLine = `
+`
+const printNewLines = noOfLines => {
+    let totalLinesStr = ''
+    for ( let i = 0; i <= noOfLines; i++ )
+        totalLinesStr += newLine
 
+    console.log( totalLinesStr )
+}
 
-`)
+printNewLines( 3 )
 
 console.log(
     consoleColors.bg.Blue,
     consoleColors.fg.White,
-`                                                                                              `,
+    defaultSeparatorSpace,
     consoleColors.Reset
 )
 
 console.log(
     consoleColors.bg.Blue,
     consoleColors.fg.White,
-    `   STARTED TEST AT [ ${new Date()} ]       `,
+    defaultMessage,
     consoleColors.Reset
 )
 
 console.log(
     consoleColors.bg.Blue,
     consoleColors.fg.White,
-`                                                                                              `,
+    defaultSeparatorSpace,
     consoleColors.Reset
 )
 
-console.log(`
-`)
+printNewLines( 1 )
