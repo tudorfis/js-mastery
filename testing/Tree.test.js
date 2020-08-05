7d4242788511daa0de7c52bcebfb1cf985814edc
tudorfis <todorescu.consulting@gmail.com>
2020-08-06 00:09:00 +0300

    .

--- testing/Tree.test.js
+++ testing/Tree.test.js
@@ -26,8 +26,8 @@ unitTesting.addAssertion([
     4
 ])
 
-filesystem.root.children[0].addNode('the matrix.avi')
-filesystem.root.children[0].addNode('family guy - latest episode.avi')
+filesystem.root.children[0].addNode('/the matrix.avi')
+filesystem.root.children[0].addNode('/family guy - latest episode.avi')
 
 unitTesting.addAssertion([
     'filesystem.root.count() === 6',
@@ -35,6 +35,44 @@ unitTesting.addAssertion([
     filesystem.root.count(),
     6
 ])
+
+unitTesting.addAssertion([
+    `filesystem.add( '/documents/personal/tax.docx' )`,
+    `filesystem.add( '/documents/personal/tax.docx' )`,
+    filesystem.add( '/documents/personal/tax.docx' ),
+    undefined
+])
+
+filesystem.add('/games/cod.exe')
+filesystem.add('/games/cod2.exe')
+filesystem.add('/games/fps/counter-strike.exe')
+
+unitTesting.addAssertion([
+    `JSON.stringify( filesystem.root )`,
+    `JSON.stringify( filesystem.root )`,
+    JSON.stringify( filesystem.root ),
+    `{"value":"/","children":[{"value":"/videos","children":[{"value":"/the matrix.avi","children":[]},{"value":"/family guy - latest episode.avi","children":[]}]},{"value":"/music","children":[]},{"value":"/games","children":[{"value":"/cod.exe","children":[]},{"value":"/cod2.exe","children":[]},{"value":"/fps","children":[{"value":"/counter-strike.exe","children":[]}]}]},{"value":"/documents","children":[{"value":"/personal","children":[{"value":"/tax.docx","children":[]}]}]}]}`
+])
+
+// filesystem.print()
+
+filesystem.delete('/games/cod.exe')
+filesystem.delete('/documents/personal/tax.docx')
+
+unitTesting.addAssertion([
+    `filesystem.delete('/games/cod.exe')`,
+    `JSON.stringify( filesystem.root )`,
+    JSON.stringify( filesystem.root ),
+    `{"value":"/","children":[{"value":"/videos","children":[{"value":"/the matrix.avi","children":[]},{"value":"/family guy - latest episode.avi","children":[]}]},{"value":"/music","children":[]},{"value":"/games","children":[{"value":"/cod2.exe","children":[]},{"value":"/fps","children":[{"value":"/counter-strike.exe","children":[]}]}]},{"value":"/documents","children":[{"value":"/personal","children":[]}]}]}`
+])
+
+
+filesystem.delete('/videos/the matrix.avi')
+filesystem.delete('/videos/family guy - latest episode.avi')
+filesystem.delete('/games/cod2.exe')
+filesystem.delete('/games/fps/counter-strike.exe')
+
+// filesystem.print()
 
 ////////////////////////////////
 unitTesting.printResults()
